abcfd4c86dd875ec77058cdab7770ca426bbae24
Anouar Belhassan <135267395+BelAnouar@users.noreply.github.com>
2025-03-18 13:19:56 +0000

    Add ActionHistorique entity

--- src/main/java/org/example/ewastev0_1/domain/entites/ActionHistorique.java
+++ src/main/java/org/example/ewastev0_1/domain/entites/ActionHistorique.java
@@ -1,4 +1,36 @@
 package org.example.ewastev0_1.domain.entites;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "actions_historique")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ActionHistorique {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    private String actionType;
+    private String description;
+    private Integer pointsGagnes;
+    private LocalDateTime dateAction;
+
+    @ManyToOne
+    @JoinColumn(name = "ewaste_id")
+    private Ewaste ewaste;
+
+
 }
