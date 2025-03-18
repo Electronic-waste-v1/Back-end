d8970683b7aca5804450526f618ad8b12beb2022
Anouar Belhassan <135267395+BelAnouar@users.noreply.github.com>
2025-03-18 13:20:12 +0000

    Add ActionHistoriqueRepository

--- src/main/java/org/example/ewastev0_1/repository/ActionHistoriqueRepository.java
+++ src/main/java/org/example/ewastev0_1/repository/ActionHistoriqueRepository.java
@@ -1,4 +1,16 @@
 package org.example.ewastev0_1.repository;
 
-public interface ActionHistoriqueRepository {
+
+import org.example.ewastev0_1.domain.entites.ActionHistorique;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ActionHistoriqueRepository extends JpaRepository<ActionHistorique, Integer> {
+    List<ActionHistorique> findByUserIdAndActionType(Integer userId, String actionType);
+
+    List<ActionHistorique> findByActionType(String actionType);
+
 }
