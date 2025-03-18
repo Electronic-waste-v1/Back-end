237b9708894e61f75ca32e6f878e108193ffd1b4
Anouar Belhassan <135267395+BelAnouar@users.noreply.github.com>
2025-03-18 13:20:22 +0000

    Add AnnonceRepository

--- src/main/java/org/example/ewastev0_1/repository/AnnonceRepository.java
+++ src/main/java/org/example/ewastev0_1/repository/AnnonceRepository.java
@@ -1,7 +1,17 @@
 package org.example.ewastev0_1.repository;
 
 import org.example.ewastev0_1.domain.entites.Annonce;
+import org.example.ewastev0_1.domain.entites.Enum.Etat;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 public interface AnnonceRepository extends JpaRepository<Annonce, Integer> {
+
+    List<Annonce> findByEwasteCategorie(String category);
+
+    List<Annonce> findByPrixBetween(BigDecimal minPrice, BigDecimal maxPrice);
+
+    List<Annonce> findByEtat(Etat condition);
 }
