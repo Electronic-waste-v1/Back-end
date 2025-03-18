d7f6ed6c3bb756a37e55577776db30f36640916f
Anouar Belhassan <135267395+BelAnouar@users.noreply.github.com>
2025-03-18 13:20:50 +0000

    Add AnnonceService interface

--- src/main/java/org/example/ewastev0_1/domain/entites/AnnonceImage.java
+++ src/main/java/org/example/ewastev0_1/domain/entites/AnnonceImage.java
@@ -1,4 +1,26 @@
 package org.example.ewastev0_1.domain.entites;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class AnnonceImage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+    private String type;
+    private String path;
+
+    @ManyToOne
+    @JoinColumn(name = "annonce_id")
+    private Annonce annonce;
 }
